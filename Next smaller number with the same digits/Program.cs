536e199b59aef95a9a143c2954161b3250b08221
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-08-01 10:56:53 +0300

    Добавьте файлы проекта.

---
+++ Next smaller number with the same digits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static void Main()
+    {
+        // Test
+        var t = NextSmaller(153);
+        // ...should return 135
+    }
+
+    public static long NextSmaller(long n)
+    {
+        string nAsStr = n.ToString();
+
+        for (int i = nAsStr.Length - 1; i > 0; i--)
+        {
+            if (nAsStr[i] >= nAsStr[i - 1])
+                continue;
+
+            char nextNumber = nAsStr.Last(c => c < nAsStr[i - 1]);
+            var lastNumbers = nAsStr[(i - 1)..].OrderByDescending(c => c).ToList();
+            lastNumbers.Remove(nextNumber);
+            long result = long.Parse(string.Join("", nAsStr[0..(i - 1)], nextNumber, string.Join("", lastNumbers)));
+
+            if (result.ToString().Length == nAsStr.Length)
+                return result;
+        }
+
+        return -1;
+    }
+}
